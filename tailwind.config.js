b194f13350cf9eb63d3d50d2d4e7eda7521b869f
John Kim A. Querobines <querobinesj@gmail.com>
2022-03-12 21:47:37 +0800

    palette config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FCF3CC",
+        alt: "#FFE900",
+        container: "#FFF",
+        body: "#FAF8F4",
+        "dark-primary": "",
+        darkContainer: "#202225",
+        darkBody: "#0D0C0C",
+      },
+    },
   },
   plugins: [],
 };
